d489d60d82803f0efcdaf522ea99d3b557c95c2c
evanandrewson <evanandrewson@gmail.com>
2019-10-03 14:36:01 -0700

    created post route and test for crystals

---
+++ e2e/__tests__/crystals.test.js
@@ -0,0 +1,49 @@
+const request = require('../request');
+const { dropCollection } = require('../db');
+const { signupUser } = require('../data-helpers');
+
+describe('Crystals API', () => {
+  beforeEach(() => dropCollection('users'));
+  beforeEach(() => dropCollection('crystals'));
+
+  let user = null;
+  beforeEach(() => {
+    return signupUser().then(newUser => (user = newUser));
+  });
+
+  const crystal = {
+    name: 'Amethyst',
+    description: 'purple and amazing',
+    price: 5.0,
+    category: 'quartz'
+  };
+
+  it('posts a crystal for this user', () => {
+    return request
+      .post('/api/crystals')
+      .set('Authorization', user.token)
+      .send(crystal, user)
+      .expect(200)
+      .then(({ body }) => {
+        console.log(body);
+        expect(body.owner).toBe(user._id);
+        expect(body).toMatchInlineSnapshot(
+          {
+            _id: expect.any(String),
+            owner: expect.any(String)
+          },
+          `
+          Object {
+            "__v": 0,
+            "_id": Any<String>,
+            "category": "quartz",
+            "description": "purple and amazing",
+            "name": "Amethyst",
+            "owner": Any<String>,
+            "price": 5,
+          }
+        `
+        );
+      });
+  });
+});
